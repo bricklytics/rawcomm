edfa5ab04b8c8f0097e25a67069b5d741ccd0637
nordael <jleandro.martins@gmail.com>
2025-05-19 15:34:38 -0300

    feat: created base protocol interface

---
+++ protocollayer/feature/base/include/IBaseProtocol.h
@@ -0,0 +1,23 @@
+//
+// Created by julio-martins on 5/18/25.
+//
+
+#ifndef IBASEPROTOCOL_H
+#define IBASEPROTOCOL_H
+
+#include <cstdint>
+#include <vector>
+#include <string>
+#include "../../../../flowcontrollayer/feature/base/include/PacketType.h"
+#include "FileType.h"
+
+class IBaseProtocol {
+public:
+    virtual ~IBaseProtocol() = default;
+    virtual bool sendMsg(PacketUtils::PacketType type, const std::vector<uint8_t>& data) = 0;
+    virtual bool sendFile(FileUtils::FileType type, const std::string& filePath) = 0;
+    virtual std::vector<uint8_t>receiveMsg() = 0;
+    virtual bool receiveFile(const std::string& filePath) = 0;
+};
+
+#endif //IBASEPROTOCOL_H
