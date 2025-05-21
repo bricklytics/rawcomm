674ce43251f2c22a3794e2c053c5edf076f5bf99
nordael <jleandro.martins@gmail.com>
2025-05-21 12:12:00 -0300

    feat: added grid utils class wrapper and direction

--- presentationlayer/feature/server/include/GridUtils.h
+++ presentationlayer/feature/server/include/GridUtils.h
@@ -4,8 +4,35 @@
 
 #ifndef GRIDUTILS_H
 #define GRIDUTILS_H
-const int GRID_SIZE = 8;
-const int LOG_SIZE = 10;
+
+#include <ncurses.h>
+
+#include "../../../../flowcontrollayer/feature/base/include/PacketType.h"
+
+constexpr int GRID_SIZE = 8;
+constexpr int LOG_SIZE = 10;
+
+enum class Direction {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+};
+
+class GridUtils {
+public:
+    static int toInt(const uint8_t cmd) {
+        int key = 0;
+        switch (cmd) {
+            case Direction::UP: key = KEY_UP; break;
+            case Direction::DOWN: key = KEY_DOWN; break;
+            case Direction::LEFT: key = KEY_LEFT; break;
+            case Direction::RIGHT: key = KEY_RIGHT; break;
+            default: key = 0; break;
+        }
+        return key;
+    }
+};
 
 class Position {
 public:
