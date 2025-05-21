78c18d287bffa99fa5f5451dbc6f2442103e03aa
nordael <jleandro.martins@gmail.com>
2025-05-21 19:08:41 -0300

    refactor: added quit command; added hash function to allow Position object be key in unordered_map

--- presentationlayer/feature/server/include/GridUtils.h
+++ presentationlayer/feature/server/include/GridUtils.h
@@ -16,7 +16,8 @@ enum class Direction {
     UP,
     DOWN,
     LEFT,
-    RIGHT
+    RIGHT,
+    QUIT
 };
 
 class GridUtils {
@@ -28,23 +29,28 @@ public:
             case Direction::DOWN: key = KEY_DOWN; break;
             case Direction::LEFT: key = KEY_LEFT; break;
             case Direction::RIGHT: key = KEY_RIGHT; break;
+            case Direction::QUIT: key = 'q'; break;
             default: key = 0; break;
         }
         return key;
     }
 };
 
-class Position {
-public:
+struct Position {
     int x, y;
 
-    bool operator<(const Position &other) const {
-        return (y * GRID_SIZE + x) < (other.y * GRID_SIZE + other.x);
-    }
-
-    bool operator==(const Position &other) const {
+    bool operator==(const Position& other) const {
         return x == other.x && y == other.y;
     }
 };
+
+namespace std {
+    template <>
+    struct hash<Position> {
+        size_t operator()(const Position& p) const noexcept {
+            return hash<int>()(p.x) ^ (hash<int>()(p.y) << 1);
+        }
+    };
+}
 
 #endif //GRIDUTILS_H
