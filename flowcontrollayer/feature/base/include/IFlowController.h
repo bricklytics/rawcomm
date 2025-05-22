503280590f3459355b6214ab2a7cb232962e3a58
nordael <jleandro.martins@gmail.com>
2025-05-22 13:24:53 -0300

    fix: adjusted destructor

--- flowcontrollayer/feature/base/include/IFlowController.h
+++ flowcontrollayer/feature/base/include/IFlowController.h
@@ -7,14 +7,13 @@
 
 #include <vector>
 #include <cstdint>
-#include "PacketType.h"
 
 class IFlowController {
 public:
     uint8_t packet_type = 0x00;
     uint8_t current_seq = 0xFF;
 
-    virtual ~IFlowController();
+    virtual ~IFlowController() = default;
 
     virtual bool dispatch(const std::vector<uint8_t>& data) = 0;
     virtual std::vector<uint8_t> receive() = 0;
