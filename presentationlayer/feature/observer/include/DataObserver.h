c59bccda43d8f3e261eca6c5d4accc13a7df8bd3
nordael <jleandro.martins@gmail.com>
2025-05-22 22:44:41 -0300

    refactor:removed unnecessary mutex lock

--- presentationlayer/feature/observer/include/DataObserver.h
+++ presentationlayer/feature/observer/include/DataObserver.h
@@ -6,43 +6,30 @@
 #define DATAOBSERVER_H
 
 #include <functional>
-#include <vector>
 #include <optional>
-#include <mutex>
 
 template <typename T>
 class DataObserver {
 public:
-    using callback = std::function<void(const T&)>;
+    using Callback = std::function<void(const T&)>;
 
     void post(const T& value) {
-        std::lock_guard lock(mutex_);
         data_ = value;
-        notifyObservers();
+        if (observer_) {
+            observer_.value()(data_.value());
+        }
     }
 
-    void observe(callback callback) {
-        std::lock_guard lock(mutex_);
-        observers_.push_back(callback);
-
-        // If data already posted, call observer immediately
-        if (data_.has_value()) {
-            callback(data_.value());
+    void observe(Callback callback) {
+        observer_ = std::move(callback);
+        if (data_) {
+            observer_.value()(data_.value());
         }
     }
 
 private:
-    void notifyObservers() {
-        for (auto& observer : observers_) {
-            if (data_.has_value()) {
-                observer(data_.value());
-            }
-        }
-    }
-
-    std::vector<callback> observers_;
     std::optional<T> data_;
-    std::mutex mutex_;
+    std::optional<Callback> observer_;
 };
 
 #endif //DATAOBSERVER_H
